52302979e19edfab782b0ef49c493338afa2b5d0
SilviaFuentes <silvia.kfuentes@gmail.com>
2025-08-20 18:23:24 -0500

    Variables como nombrarlas

---
+++ 01_variables.py
@@ -0,0 +1,26 @@
+#Variales validas
+""""
+firstname
+lastname
+age
+country
+city
+first_name
+last_name
+capital_city
+_if # if we want to use reserved word as a variable
+year_2019
+year2019
+current_year_2019
+num1
+num2
+
+"""
+#Variales no validas
+
+"""
+first-name
+num-1
+1num
+"""
+
