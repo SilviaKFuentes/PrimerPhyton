37ba781ebc95489157545bfd8fc33d51cc568b75
SilviaFuentes <silvia.kfuentes@gmail.com>
2025-08-20 19:32:27 -0500

    lopps basic for

--- 09_loops.py
+++ 09_loops.py
@@ -1,17 +1,31 @@
 
-#While
+#For
 
-my_condition = 0
+# Lista
+my_list = [35, 24, 62, 52, 30, 30, 17]
 
+for element in my_list:
+    print("Lista:", element)
 
-"""
-while my_condition < 10:
-    print(my_condition)
+# Tupla
+my_tuple = (35, 1.77, "Brais", "Moure", "Brais")
 
-bucle infinito porque 0 siempre será menor que 10
-"""
+for element in my_tuple:
+    print("Tupla:", element)
 
-while my_condition < 10:
-    print(my_condition)
-    my_condition += 2
+# Conjunto (set)
+my_set = {"Brais", "Moure", 35}
 
+for element in my_set:
+    print("Set:", element)
+
+# Diccionario
+my_dict = {"Nombre": "Brais", "Apellido": "Moure", "Edad": 35, 1: "Python"}
+
+# Recorriendo solo las claves
+for key in my_dict:
+    print("Diccionario (clave):", key)
+
+# Recorriendo claves y valores
+for key, value in my_dict.items():
+    print("Diccionario (clave-valor):", key, "→", value)
