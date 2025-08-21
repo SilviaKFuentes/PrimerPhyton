217546abafd7db411ef0a52f1b4fbb89b5bda34e
SilviaFuentes <silvia.kfuentes@gmail.com>
2025-08-20 21:14:12 -0500

    modules basic

---
+++ 13_modules.py
@@ -0,0 +1,18 @@
+# Importando el módulo completo
+import my_module
+
+my_module.sumValue(5, 3, 1)
+my_module.printValue("Hola Python!")
+
+# Importando solo funciones específicas del módulo
+from my_module import sumValue, printValue
+
+sumValue(5, 3, 1)
+printValue("Hola python")
+ 
+
+import math
+
+print(math.pi)
+
+# buscar mas funciones matematicas con math. 
---
+++ my_module.py
@@ -0,0 +1,5 @@
+def sumValue(a, b, c):
+    print("La suma es:", a + b + c)
+
+def printValue(text):
+    print("Texto recibido:", text)
